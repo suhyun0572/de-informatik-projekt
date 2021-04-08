54d666d523cd62381e0ba0320a7de21f698b0ee0
suhyun0572 <suhyun0572@gmail.com>
2021-04-08 16:12:38 +0200

    Login popup without comments

--- www/js/main.js
+++ www/js/main.js
@@ -1,8 +1,13 @@
 const header = document.querySelector('#header');
+const richtSide = document.querySelector('div.right_side');
+const loginPopUp = document.querySelector('article.login');
+const contents = document.querySelector('.contents');
+const close = document.querySelector('.close');
+
 
 
 function onScroll() {
-    console.log(window.pageYOffset);
+    //console.log(window.pageYOffset);
     if (window.pageYOffset >= 10) {
         header.classList.add('on');
     }
@@ -11,5 +16,16 @@ function onScroll() {
     }
 
 }
+
+
+function popup() {
+    console.log("hit!");
+    loginPopUp.classList.toggle('on');
+    contents.classList.toggle('on');
+}
+
+richtSide.addEventListener('click', popup);
+close.addEventListener('click', popup);
+
 
 window.addEventListener('scroll', onScroll);
