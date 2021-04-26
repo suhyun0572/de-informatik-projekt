26bde49f4168bc136dc31cb493f7f7f9ba0fee54
Hana Joo <hanajoo@google.com>
2021-04-26 23:48:49 +0200

    Simple display of the gameRoom in index.html

---
+++ lib/frontend/scripts/frame.ts
@@ -0,0 +1,30 @@
+
+const GAME_CANVAS_ID = 'gameRoom';
+/**
+ * Empty whatever's in the body of the html and return the body element
+ */
+function emptyBody() : HTMLElement {
+    const body = document.getElementById("body");
+    while (body.lastElementChild) {
+        body.removeChild(body.lastElementChild);
+    }
+    return body;
+}
+/**
+ * Create a game room canvas and replace the HTML body with it
+ */
+function displayGameRoom(){
+    function createCenterCanvas() : HTMLElement {
+        const center = document.createElement('center');
+        const canvas = document.createElement('canvas');
+        canvas.setAttribute('class', GAME_CANVAS_ID);
+        canvas.setAttribute('id', GAME_CANVAS_ID);
+        center.appendChild(canvas);
+        return center;
+    }
+    const body = emptyBody();
+    const canvas = createCenterCanvas();
+    body.appendChild(canvas);
+}
+
+window.onload = displayGameRoom;
